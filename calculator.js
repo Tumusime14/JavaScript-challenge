481f62ab0769f31a3b424ca1de65ee3e8e11909b
tumusime_david <tumusimedavid14@gmail.com>
2024-12-13 17:11:43 +0200

    dev/ Simple calculator function using switch statement

---
+++ calculator.js
@@ -0,0 +1,22 @@
+function calculator(num1, num2, operator) {
+	let result;
+	switch (operator) {
+		case "+":
+			result = num1 + num2;
+			break;
+		case "-":
+			result = num1 - num2;
+			break;
+		case "*":
+			result = num1 * num2;
+			break;
+		case "/":
+			result = num1 / num2;
+			break;
+		default:
+			result = "Invalid value and/or operator";
+	}
+	console.log(result);
+	return result;
+}
+
