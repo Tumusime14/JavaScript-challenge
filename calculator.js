6ba14cd9d63f11bc1c6ed0d6fd195b651f0ab1d1
tumusime_david <tumusimedavid14@gmail.com>
2024-12-16 00:07:23 +0200

    dev - Simple calculator using HTML/CSS and JS

--- calculator.js
+++ calculator.js
@@ -1,22 +1,23 @@
-function calculator(num1, num2, operator) {
-	let result;
+document.getElementById("btn").addEventListener("click", function () {
+	let num1 = document.querySelector(".num1").value;
+	let num2 = document.querySelector(".num2").value;
+	let result = document.querySelector(".result");
+
+	let operator = document.getElementById("selectOp").value;
 	switch (operator) {
 		case "+":
-			result = num1 + num2;
+			result.innerHTML = Number(num1) + Number(num2);
 			break;
 		case "-":
-			result = num1 - num2;
+			result.innerHTML = Number(num1) - Number(num2);
 			break;
 		case "*":
-			result = num1 * num2;
+			result.innerHTML = Number(num1) * Number(num2);
 			break;
 		case "/":
-			result = num1 / num2;
+			result.innerHTML = Number(num1) / Number(num2);
 			break;
 		default:
 			result = "Invalid value and/or operator";
 	}
-	console.log(result);
-	return result;
-}
-
+});
