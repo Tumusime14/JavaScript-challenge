0c15fe4756bf151ca697104edd5135787ae2727f
tumusime_david <tumusimedavid14@gmail.com>
2024-12-23 09:35:18 +0200

    Learning OOP/- Getters and Setters

--- BroCode/index.js
+++ BroCode/index.js
@@ -1,20 +1,55 @@
-class Product {
-	constructor(name, price) {
-		this.name = name;
-		this.price = price;
+class Rectangle {
+	constructor(width, height) {
+		this.width = width;
+		this.height = height;
 	}
-	displayProduct() {
-		console.log(`Product: ${this.name}`);
-		console.log(`Price: $${this.price}`);
+	set width(newWidth) {
+		if (newWidth > 0) {
+			this._width = newWidth;
+		} else {
+			console.error("Width must be a posotive number");
+		}
 	}
-	priceWithTax() {
-		return this.price + this.price * salesTax;
+	set height(newHeight) {
+		if (newHeight > 0) {
+			this._height = newHeight;
+		} else {
+			console.error("Height must be a positive number");
+		}
+	}
+	get width() {
+		return `${this._width.toFixed(1)}cm`;
+	}
+	get height() {
+		return `${this._height.toFixed(1)}cm`;
+	}
+	get area() {
+		return `${(this._width * this._height).toFixed(1)}cm^2`;
 	}
 }
 
-const product1 = new Product("T-Shirt", 100);
-product1.displayProduct();
+const rectange = new Rectangle(5, 7);
+console.log(rectange.width);
+console.log(rectange.height);
+console.log(rectange.area);
 
-const salesTax = 0.05;
-total = product1.priceWithTax();
-console.log(total);
+// class Product {
+// 	constructor(name, price) {
+// 		this.name = name;
+// 		this.price = price;
+// 	}
+// 	displayProduct() {
+// 		console.log(`Product: ${this.name}`);
+// 		console.log(`Price: $${this.price}`);
+// 	}
+// 	priceWithTax() {
+// 		return this.price + this.price * salesTax;
+// 	}
+// }
+
+// const product1 = new Product("T-Shirt", 100);
+// product1.displayProduct();
+
+// const salesTax = 0.05;
+// total = product1.priceWithTax();
+// console.log(total);
